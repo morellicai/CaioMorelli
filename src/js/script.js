51e1ae0a80bb635e347eb95deb0748d6afd3e75f
Caio Morelli <caiomorelli.it@gmail.com>
2025-02-25 16:29:51 +0000

    Mudando projeto para meu projeto pessoal

--- src/js/script.js
+++ src/js/script.js
@@ -15,13 +15,13 @@ window.addEventListener('scroll', () => {
 });
 // Todos os IDs dos cartões
 const cards = [
-    "card-desktop", 
-    "card-web", 
-    "card-mobile", 
-    "card-ecommerce", 
-    "card-design",
-    "card-suporte", 
-    "card-gestao"
+    "card-projeto1",
+    "card-projeto2",
+    "card-projeto3",
+    "card-projeto4",
+    "card-projeto5",
+    "card-projeto6",
+    "card-projeto7"
 ];
 // Função dos Serviços
 function card(event, type) {
@@ -50,56 +50,56 @@ document.querySelectorAll('.list-services').forEach(button => {
     });
 });
 
-function devs(event, type){
-    event.preventDefault();
-    const cards = [
-        "card-eddie", 
-        "card-felipe", 
-        "card-elias", 
-        "card-dmitri",
-        "card-pedro",
-        "card-caio"
-    ];
-    cards.forEach(cardId => {
-        const cardElement = document.getElementById(cardId);
-        cardElement.style.display = (cardId === `card-${type}`) ? "flex" : "none";
-    });
-}
+// function devs(event, type){
+//     event.preventDefault();
+//     const cards = [
+//         "card-eddie",
+//         "card-felipe",
+//         "card-elias",
+//         "card-dmitri",
+//         "card-pedro",
+//         "card-caio"
+//     ];
+//     cards.forEach(cardId => {
+//         const cardElement = document.getElementById(cardId);
+//         cardElement.style.display = (cardId === `card-${type}`) ? "flex" : "none";
+//     });
+// }
 
-// Adicionando os event listeners de forma genérica
-document.querySelectorAll('.desenvolvedor').forEach(button => {
-    button.addEventListener('click', function(event) {
-        event.preventDefault();
-    });
-    button.addEventListener('mouseover', function(event) {
-        event.preventDefault();
+// // Adicionando os event listeners de forma genérica
+// document.querySelectorAll('.desenvolvedor').forEach(button => {
+//     button.addEventListener('click', function(event) {
+//         event.preventDefault();
+//     });
+//     button.addEventListener('mouseover', function(event) {
+//         event.preventDefault();
 
-        document.querySelectorAll('.desenvolvedor').forEach(activeButton => activeButton.classList.remove('ative'));
+//         document.querySelectorAll('.desenvolvedor').forEach(activeButton => activeButton.classList.remove('ative'));
 
-        button.classList.add('ative');
+//         button.classList.add('ative');
 
-        const type = button.getAttribute('data-type');
-        devs(event, type);
-    })
-});
+//         const type = button.getAttribute('data-type');
+//         devs(event, type);
+//     })
+// });
 
 // Animmação de entrada
 
-// const elements = document.querySelectorAll('.sectionsSite');
-// console.log(elements)
-// const myObserver = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add('show');
-//         } else {
-//             entry.target.classList.remove('show');
-//         }
-//     });
-// });
+const elements = document.querySelectorAll('.sectionsSite');
+console.log(elements)
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
 
-// elements.forEach((element) => {
-//     myObserver.observe(element);
-// });
+elements.forEach((element) => {
+    myObserver.observe(element);
+});
 
 // Sobrescrita do formulário
 const form = document.getElementById('form');
